29c397f0b66eeee4e26c9f8a268c6401d77599f3
Anton <perch.anton@gmail.com>
2023-08-22 04:54:40 +0300

    feat: added some DB connection description

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,15 +7,20 @@ import userRoutesJSON from './api/routes/usersJSON';
 
 const app = express();
 
+// If you want to connect your mongoDB table, create .env file inside ./server folder and add params as below.
+// I'm using MongoDB Atlas free plan for learning propose. You can try it from this link:
+// https://www.mongodb.com/atlas
+
 const mongoUsername = process.env.MONGO_DB_USER;
 const mongoPassword = process.env.MONGO_DB_PASSWORD;
+const mongoTable = process.env.MONGO_DB_TABLENAME;
 
-const isAcceptable = mongoUsername && mongoPassword;
+const isAcceptable = mongoUsername && mongoPassword && mongoTable;
 
 if (isAcceptable) {
   mongoose
     .connect(
-      `mongodb+srv://${mongoUsername}:${mongoPassword}@node-rest.92tl9gn.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${mongoUsername}:${mongoPassword}@${mongoTable}.92tl9gn.mongodb.net/?retryWrites=true&w=majority`
     )
     .then(() => {
       console.log('MongoDB connection established!');
